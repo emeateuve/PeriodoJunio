4d62f20dbc9b48c34acddb90ea4388a0f772d6b5
Emeateuve <emeateuve@gmail.com>
2018-03-15 13:45:06 +0100

    .

--- JAVASCRIPT/Relacion 1/relacion1.js
+++ JAVASCRIPT/Relacion 1/relacion1.js
@@ -1,12 +1,30 @@
 /* Ejercicios 3-8 */
 
-let numeros = [5, 3, 2];
+let numeros = [5, 3, 2, 4, 7, 10, 20];
+let palabras = [
+    'Blow',
+    'Kiss',
+    'Firegun',
+    'abcde'
+];
 
 function max(array) {
-    for (let i = 0; i < array.length; i++) {
-        console.log(array[i]);
-
-    }
+    return Math.max.apply(null, array);
 }
 
-max(numeros);
+
+
+function filtraPalabrasMayores(array, valor) {
+    let devueltas = [];
+    for (let i = 0; i < array.length; i++ ) {
+        if (array[i].length >= valor){
+            devueltas.push(array[i]);
+        }
+    }
+    return devueltas;
+}
+
+
+
+console.log(max(numeros)); // Llamada ejercicio 3
+console.log(filtraPalabrasMayores(palabras, 5)) // Llamada ejercicio 8
