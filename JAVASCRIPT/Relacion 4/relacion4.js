57b09884b630a9480fb2c2a2be22b5f87680b555
emeateuve <emeateuve@gmail.com>
2018-03-15 23:21:24 +0100

    Relacion 4 terminada

--- JAVASCRIPT/Relacion 4/relacion4.js
+++ JAVASCRIPT/Relacion 4/relacion4.js
@@ -1,28 +1,30 @@
 /* Ejercicios 5,6 */
 
-
 function eliminaElemento(elemento) {
     var padre = document.getElementById('padre');
     var hijo = document.getElementById(elemento)
     padre.removeChild(hijo);
 };
 
-
-
 function crearTabla(array) {
-
+    let esFila = false;
     $('#tabla').append('<table></table>');
 
     for (let i = 0; i < array.length; i++){
-        $('#tabla').append('<th>' + array[i] + '</th>');
         if(array[i] === '/'){
+            esFila = true;
             i++
             $('#tabla').append('<tr>');
+        }
+
+        if(esFila === true){
             $('#tabla').append('<td>' + array[i] + '</td>');
         }
 
+        if(esFila === false){
+            $('#tabla').append('<th>' + array[i] + '</th>');
+        }
     }
-    
 }
 
-crearTabla(['primero','segundo','tercero','/','despues','de','la barra'])
+crearTabla(['primero','segundo','tercero','/','despues','de','la barra','/','despues','de','la barra','/','despues','de','la barra','/','de','la barra'])
