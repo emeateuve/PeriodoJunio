12387b8b87ea3e589850d5c0942009931591310b
emeateuve <emeateuve@gmail.com>
2018-03-15 16:55:34 +0100

    Agenda hecha

--- JAVASCRIPT/Relacion 2/relacion2.js
+++ JAVASCRIPT/Relacion 2/relacion2.js
@@ -15,6 +15,14 @@ class Agenda {
             email: email
         });
         this.id++
+
+        $('#nombreHTML').val('');
+        $('#direccionHTML').val('');
+        $('#telefonoHTML').val('');
+        $('#emailHTML').val('');
+
+        // this.mostrarTodos()
+
     }
 
     mostrarContacto(nombre) {
@@ -34,10 +42,16 @@ class Agenda {
     }
 
     borrarContacto(nombre) {
+        let contactoBorrado = false;
         for (let i = 0; i < this.contactos.length; i++) {
             if (this.contactos[i].nombre === nombre) {
+                document.getElementById('borrado').innerHTML = 'Se ha borrado ' + nombre + ' satisfactoriamente'
                 this.contactos.splice(i, 1)
+                contactoBorrado = true;
             }
+        }
+        if(contactoBorrado == false){
+            document.getElementById('borrado').innerHTML = 'No se ha encontrado nadie con ese nombre.'
         }
     }
 }
@@ -48,7 +62,7 @@ miAgenda.introducirContacto('Migue', 'Piso', '619619619', 'emeateuve@gmail.com')
 miAgenda.introducirContacto('Antonio', 'Casa', '666666666', 'antonio@gmail.com');
 miAgenda.introducirContacto('Jose', 'PISO', '123123123', 'jose@gmail.com');
 miAgenda.introducirContacto('Pepe', 'Casa', '456456456', 'pepe@gmail.com');
-
-console.log(miAgenda.mostrarContacto('Migue'));
-console.log(miAgenda.mostrarTodos());
-console.log(miAgenda.borrarContacto('Migue'));
+//
+// console.log(miAgenda.mostrarContacto('Migue'));
+// console.log(miAgenda.mostrarTodos());
+// console.log(miAgenda.borrarContacto('Migue'));
