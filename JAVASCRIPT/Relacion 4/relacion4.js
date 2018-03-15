6612b6c7cd0e3a318b688249408996dd667848cd
emeateuve <emeateuve@gmail.com>
2018-03-15 20:13:03 +0100

    Haciendo tabla

--- JAVASCRIPT/Relacion 4/relacion4.js
+++ JAVASCRIPT/Relacion 4/relacion4.js
@@ -1,5 +1,28 @@
 /* Ejercicios 5,6 */
 
-function eliminaElemento() {
+
+function eliminaElemento(elemento) {
+    var padre = document.getElementById('padre');
+    var hijo = document.getElementById(elemento)
+    padre.removeChild(hijo);
+};
+
+
+
+function crearTabla(array) {
+
+    $('#tabla').append('<table></table>');
+
+    for (let i = 0; i < array.length; i++){
+        $('#tabla').append('<th>' + array[i] + '</th>');
+        if(array[i] === '/'){
+            i++
+            $('#tabla').append('<tr>');
+            $('#tabla').append('<td>' + array[i] + '</td>');
+        }
+
+    }
     
 }
+
+crearTabla(['primero','segundo','tercero','/','despues','de','la barra'])
