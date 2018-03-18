2e5324b8d52828f69c7fcf0a8297ef0407dfbba3
emeateuve <emeateuve@gmail.com>
2018-03-18 01:10:11 +0100

    Es una muy buena hora y muy buen momento para trabajar. Relación 5 ejercicios 2-3 hechos

--- JAVASCRIPT/Relacion 5/relacion5.js
+++ JAVASCRIPT/Relacion 5/relacion5.js
@@ -1,5 +1,17 @@
 /* Ejercicios 2-3-4-5 */
 
-function insertarFila(posicion) {
+
+
+function insertarFila() {
+    var posicion = document.getElementById("inputFila").value;
+    var latabla = document.getElementById("mitabla");
+    latabla.insertRow(posicion).innerHTML = "TABLA INSERTADA"
+}
+
+function actualizarCelda() {
+    var fila = document.getElementById("posicionFila").value;
+    var columna = document.getElementById("posicionColumna").value;
+    var texto = document.getElementById("posicionTexto").value;
+    document.getElementById("mitabla").rows[fila].cells[columna].innerHTML = texto
 
 }
