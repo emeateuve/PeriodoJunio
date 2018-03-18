b8036491dad11b4a48269f22694cf848d066a3a6
emeateuve <emeateuve@gmail.com>
2018-03-18 02:34:56 +0100

    Relacion 5 ejercicio 4

--- JAVASCRIPT/Relacion 5/relacion5.js
+++ JAVASCRIPT/Relacion 5/relacion5.js
@@ -1,7 +1,5 @@
 /* Ejercicios 2-3-4-5 */
 
-
-
 function insertarFila() {
     var posicion = document.getElementById("inputFila").value;
     var latabla = document.getElementById("mitabla");
@@ -13,5 +11,23 @@ function actualizarCelda() {
     var columna = document.getElementById("posicionColumna").value;
     var texto = document.getElementById("posicionTexto").value;
     document.getElementById("mitabla").rows[fila].cells[columna].innerHTML = texto
+}
 
+var existe = false;
+
+function insertarDropdown() {
+    var contenedor = document.getElementById("dropdown");
+    if (existe === false) {
+        var dropdown = document.createElement("select");
+        dropdown.setAttribute("id", "miDropdown")
+        existe = true;
+        contenedor.appendChild(dropdown)
+
+
+    }
+}
+
+function insertarOpcion() {
+    var midropdown = document.getElementById("miDropdown");
+    midropdown.options.add(new Option(document.getElementById("textoOpcion").value));
 }
